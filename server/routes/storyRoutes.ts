1ca840de45241a476a97423fd34cac9f65a134fb
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-03-20 18:51:35 +0200

    storys routes

--- server/routes/storyRoutes.ts
+++ server/routes/storyRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 export const storyRouter = Router();
 
-import { story, getStorys, getStory } from '../controllers/storyController';
+import {
+  story,
+  getMyStorys,
+  getStorys,
+  getStoryImages,
+} from '../controllers/storyController';
 import { protect } from '../middlewares/protect';
 
 storyRouter.post('/add-story', protect, story);
 storyRouter.get('/get-storys', protect, getStorys);
-storyRouter.get('/get-story', protect, getStory);
+storyRouter.get('/get-my-story', protect, getMyStorys);
+storyRouter.get('/get-story-images/:id', protect, getStoryImages);
